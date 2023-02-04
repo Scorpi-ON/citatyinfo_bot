417eb0af23d16ac07270e2ccdb3925c970076c35
Scorpi-ON <s.scorpi-on@ya.ru>
2023-02-04 13:10:55 +0300

    Выделил первоначальную авторизацию в отдельный файл

---
+++ first_auth.py
@@ -0,0 +1,14 @@
+from pyrogram import Client
+from dotenv import dotenv_values
+
+'''
+Перед запуском создайте файл ".env" со следующим содержанием:
+api_id=YOUR_API_ID
+api_hash=YOUR_API_HASH
+bot_token=YOUR_BOT_TOKEN
+
+Измените значения констант в файле и название сессии ниже на ваши.
+'''
+
+app = Client('SessionName', **dotenv_values())
+app.run()
