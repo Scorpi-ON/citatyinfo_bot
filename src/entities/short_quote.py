07c4606dbee5ccbc44690404f970a8cee9bbf9dc
Scorpi-ON <s.scorpi-on@ya.ru>
2024-01-15 01:38:41 +0300

    Добавлен метод для получения текста без заголовка, необходимый в некоторых случаях

--- src/entities/short_quote.py
+++ src/entities/short_quote.py
@@ -1,3 +1,5 @@
+import functools
+
 from src import utils
 from src.entities.base_quote import BaseQuote
 
@@ -5,11 +7,15 @@ from src.entities.base_quote import BaseQuote
 class ShortQuote(BaseQuote):
     MAX_LENGTH = 250
 
-    def __str__(self):
+    @functools.cached_property
+    def text(self) -> str:
         text = self._text
         if isinstance(text, tuple):
             text = f'{text[0]}\n\n{text[1]}'
+        return utils.trim_text(utils.optimize_text(text), ShortQuote.MAX_LENGTH)
+
+    def __str__(self):
+        text = self.text
         if self.header:
             text = f'**{self.header}**\n{text}'
-        text = utils.optimize_text(text)
         return utils.trim_text(text, ShortQuote.MAX_LENGTH)
